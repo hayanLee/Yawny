e48b13b43a7eed1f0866b2f2a7c7bc81f30ab907
hayanLee <hayanlee.dev@gmail.com>
2025-07-28 22:55:43 +0900

    chore : Tanstack Query 설치 및 세팅

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,17 +1,20 @@
 import Header from '@/components/Header';
 import Navigation from '@/components/Navigation';
+import Providers from '@/providers';
 import React from 'react';
 
-const WishLayout = ({ children }: { children: React.ReactNode }) => {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='bg-white'>
-      <div className='sticky top-0 z-10 h-[112px] p-4 bg-white'>
-        <Header />
-        <Navigation />
+    <Providers>
+      <div className='bg-white'>
+        <div className='sticky top-0 z-10 h-[112px] p-4 bg-white'>
+          <Header />
+          <Navigation />
+        </div>
+        {children}
       </div>
-      <main className='w-full h-[calc(100dvh-112px)]'>{children}</main>
-    </div>
+    </Providers>
   );
 };
 
-export default WishLayout;
+export default RootLayout;
